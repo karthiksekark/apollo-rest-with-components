4a4d4c485ba366e9dce142141495bac9a425b4a7
karthiksekark <karthiksekark@gmail.com>
2020-04-16 08:41:45 +0530

    Apollo rest with components
    Apollo rest with react query components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import "./App.css";
 
+const postQuery = gql`
+  query {
+    post @rest(type: "POSTS", path: "posts/1") {
+      title
+    }
+  }
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Query query={postQuery}>
+      {({ loading, error, data }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error Message: ${error.message}`;
+        if (data) {
+          return (
+            <div className="App">
+              <h1>Post Detail</h1>
+              <p>{data.post.title}</p>
+            </div>
+          );
+        }
+      }}
+    </Query>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { RestLink } from "apollo-link-rest";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const restLink = new RestLink({
+  uri: "http://jsonplaceholder.typicode.com/",
+});
+
+const client = new ApolloClient({
+  link: restLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
